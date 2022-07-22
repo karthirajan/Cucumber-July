86ad0ede0614a490adc62ab30492cd8b8f4a671e
karthirajan <karthirajan0706@gmail.com>
2022-07-22 20:42:43 +0800

    resolved conflicts

--- src/test/java/com/flipkart/stepdefinition/Hooks.java
+++ src/test/java/com/flipkart/stepdefinition/Hooks.java
@@ -1,7 +1,11 @@
 package com.flipkart.stepdefinition;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 import com.flipkart.resources.Commonactions;
 
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
@@ -19,9 +23,18 @@ public class Hooks extends Commonactions{
 	}
 	
 	@After
-	public void afterScenario() {
+	public void afterScenario(Scenario scenario) {
 		
 		System.out.println("After Scenario");
+		
+		if(scenario.isFailed()){
+			final byte[] screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+			scenario.embed(screenshot, "image/png");
+		}else{
+			final byte[] screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+			scenario.embed(screenshot, "image/png");
+		}
+		
 		//browser quit
 		driver.quit();
 
